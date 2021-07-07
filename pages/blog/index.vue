d131d59d6e7cbbe1f26464ce55c069a625f47d8b
kmacoders <hungkids3003@gmail.com>
2021-07-07 11:52:08 +0700

    fix: scroll body

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="blog-posts">
+  <section class="section">
     <div class="container">
       <div class="columns">
         <div class="column is-10 is-offset-1">
@@ -9,7 +9,7 @@
         </div>
       </div>
       <div class="columns">
-        <div class="column is-10 is-offset-1">
+        <div class="column">
           <Pagination
             :total="allArticles.length"
             :per-page="perPage"
@@ -58,46 +58,3 @@ import ListBlog from '@/components/organisms/ListBlog.vue'
 export default class PageBlog extends Vue {
 }
 </script>
-<style>
-.va {
-  display: flex;
-  align-items: center;
-}
-
-.blog-posts {
-  min-height: 100vh;
-  padding-top: 4rem;
-  padding-bottom: 10rem;
-}
-
-.blog-posts article {
-  padding: 1.5rem;
-}
-
-.blog-posts nav {
-  padding: 1.5rem;
-}
-
-.blog-posts .post img {
-  border-radius: 6px;
-}
-
-.blog-posts .post .post-category {
-  color: #3b7adf;
-}
-
-.blog-posts .post .post-title {
-  font-size: 2.2rem;
-}
-
-.blog-posts .post .post-excerpt {
-  line-height: 1.5rem;
-  letter-spacing: 0.03rem;
-  font-size: 1.2rem;
-  font-family: 'IBM Plex Serif', serif;
-}
-
-.blog-posts .post .featured .featured-image img {
-  height: 380px;
-}
-</style>
